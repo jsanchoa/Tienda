98d1d87481bf3b85734f244dc8e36121d4d1bfe8
Jose Sancho <jsancho60367@ufide.ac.cr>
2025-02-11 16:50:52 -0600

    Semana #4

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author joesa
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/") 
+    public String index(Model model) {
+        //model.addAttribute("attribute", "value");
+        return "index";
+    }
+    
+    @RequestMapping("/contacto") 
+    public String contacto(Model model) {
+        //model.addAttribute("attribute", "value");
+        return "contacto";
+    }
+
+}
